183e015fad0f206aa48670f452e118bddefc639f
Yuki Yamamura <126521201+yuki-yamamura@users.noreply.github.com>
2024-05-06 18:36:03 +0900

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import styles from './index.module.scss';
+
+const Home = () => {
+  return (
+    <main className={styles.main}>
+      <h1>Hello, World!</h1>
+    </main>
+  );
+};
+
+export default Home;
