da281ea8ded65ee314b563f3713001de4825b7e6
Yuki Yamamura <yuki.yamamura@icloud.com>
2024-05-09 05:53:05 +0900

    chore: learn grid layout

--- src/features/pokemon/components/Gallery/index.tsx
+++ src/features/pokemon/components/Gallery/index.tsx
@@ -1,10 +1,12 @@
 import Card from '@/features/pokemon/components/Card';
 
+import styles from './index.module.css';
+
 const Gallery = () => {
-  const pokemonIds = [1, 2, 3];
+  const pokemonIds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   return (
-    <div>
+    <div className={styles.module}>
       {pokemonIds.map((id) => (
         <Card id={id} key={id} />
       ))}
