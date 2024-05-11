7c335432efc3cfffe8e70bed24188b58f8cafb16
Yuki Yamamura <yuki.yamamura@icloud.com>
2024-05-11 10:55:23 +0900

    feat: get Pokemon from the API instead of fake data

--- src/features/pokemon/components/Card/index.tsx
+++ src/features/pokemon/components/Card/index.tsx
@@ -1,43 +1,47 @@
+'use client';
+
 import Tag from './Tag';
-import fs from 'fs/promises';
-import path from 'path';
+import useSWR from 'swr';
 
 import type { Pokemon } from '@/features/pokemon/types/Pokemon';
+import type { PokemonInfo } from '@/types/PokemonInfo';
 
 import styles from './index.module.css';
 
-type Props = {
-  id: number;
-};
+type Props = PokemonInfo;
 
-const Card = async ({ id }: Props) => {
-  // todo: write code to fetch data from the API.
-  console.log(id);
+const Card = ({ name, url }: Props) => {
+  const { data, isLoading } = useSWR(url, async (url: string) => {
+    const res = await fetch(url);
+    const data = (await res.json()) as Pokemon;
 
-  const dataPath = path.join(process.cwd(), 'data', 'ditto.json');
-  const json = (await fs.readFile(dataPath)).toString();
-  const pokemon = JSON.parse(json) as Pokemon;
-  const name = pokemon.name.slice(0, 2).toUpperCase() + pokemon.name.slice(1);
+    return data;
+  });
+  const formattedName = name.slice(0, 1).toUpperCase() + name.slice(1);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className={styles.module}>
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
-        src={pokemon.sprites.front_default}
-        alt={name}
+        src={data?.sprites.front_default}
+        alt={formattedName}
         className={styles.image}
       />
-      <div className={styles.name}>{name}</div>
+      <div className={styles.name}>{formattedName}</div>
       <div className={styles.details}>
         <ul className={styles.abilities}>
-          {pokemon.abilities.map(({ ability }) => (
+          {data?.abilities.map(({ ability }) => (
             <li key={ability.name} className={styles.ability}>
               <Tag color="coral">{ability.name}</Tag>
             </li>
           ))}
         </ul>
         <ul className={styles.types}>
-          {pokemon.types.map(({ type }) => (
+          {data?.types.map(({ type }) => (
             <li key={type.name} className={styles.type}>
               <Tag color="cornflowerblue">{type.name}</Tag>
             </li>
--- src/features/pokemon/components/Gallery/index.tsx
+++ src/features/pokemon/components/Gallery/index.tsx
@@ -1,14 +1,22 @@
 import Card from '@/features/pokemon/components/Card';
 
+import type { PokemonInfo } from '@/types/PokemonInfo';
+
 import styles from './index.module.css';
 
-const Gallery = () => {
-  const pokemonIds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+type ApiResponseType = {
+  next: string;
+  results: PokemonInfo[];
+};
+
+const Gallery = async () => {
+  const res = await fetch('https://pokeapi.co/api/v2/pokemon');
+  const data = (await res.json()) as ApiResponseType;
 
   return (
     <div className={styles.module}>
-      {pokemonIds.map((id) => (
-        <Card id={id} key={id} />
+      {data.results.map((pokemonInfo) => (
+        <Card {...pokemonInfo} key={pokemonInfo.name} />
       ))}
     </div>
   );
